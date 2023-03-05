be451a1b9c002c5c7837d781c6f67a17d9246999
misut <pkr5207@gmail.com>
2023-03-06 00:14:36 +0900

    feat: integrate with backend

---
+++ src/lib/todo.ts
@@ -0,0 +1,6 @@
+export type Todo = {
+  id: number;
+  description: string;
+  created_at: number;
+  is_finished: boolean;
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import type { Todo } from '../lib/todo';
+
+  let todos: Todo[] = [];
+
+  let value: string = '';
+
+  onMount(async () => {
+    const fetched = await fetch('/todos');
+    todos = await fetched.json();
+  });
+
+  async function handleKeydown(event: KeyboardEvent) {
+    console.log(event.key);
+    if (event.key != 'Enter') {
+      return;
+    }
+
+    const todo: Todo = {
+      id: todos.length + 1,
+      description: value,
+      created_at: new Date().getTime() / 1000,
+      is_finished: false,
+    };
+    todos = [...todos, todo];
+    await fetch('/todos', {method: 'POST', body: JSON.stringify(todo), headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}})
+  }
+</script>
+
+<h1>Todo</h1>
+
+{#each todos as todo (todo.id)}
+  <input type="checkbox" bind:group={todos} name="todos">
+  {todo.description}
+  <button>Delete</button>
+  <br>
+{/each}
+
+<br>
+
+<label>
+  Enter a new todo
+  <input bind:value on:keydown={handleKeydown}>
+</label>
